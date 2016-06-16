1329d98a7c97adff5dc215438abc6da46c78bbe8
Jordi Jolink <info@soneritics.nl>
2016-06-16 21:35:34 +0200

    Add example files

--- src/configs.php
+++ src/configs.php
@@ -1,4 +1,5 @@
 <?php
-$websiteKey = 't0Fi1tr9Nq';
-$secretKey = '28h7d7892hg7827gh78g2';
-$returnURL = 'http://jordijolink.nl/buckaroo/Sample/ReturnPage.php';
+$websiteKey = ''; // Get this at the Buckaroo Payment Plaza
+$secretKey = ''; // Get this at the Buckaroo Payment Plaza
+$returnURL = ''; // Full URL to the ReturnPage.php script
+
